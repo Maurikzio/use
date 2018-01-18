fbce70975b065164853a8e3c56437710a6640c5f
mauricio <mauritzio93@hotmail.com>
2018-01-18 20:47:55 +0200

    fixed all bugs of url images in js file

--- app/js/script.js
+++ app/js/script.js
@@ -62,21 +62,24 @@ let ivanoDescription = `Esta ciudad es la puerta de entrada a los famosos Cárpa
 Es una ciudad muy conocida ya que alberga muchos estudiantes que desean seguir carreras de ingeniería en petróleos y gas.`;
 modalLviv.onclick = function(){
     modal.style.display = 'block';
-    modalImg.style.backgroundImage = "url('../images/lviv.jpg')";  
+    modalImg.className = "the-modal-img in-lviv col-4 ";
+    // modalImg.style.background = "url('../images/lviv.jpg')";  
     cityDescription.innerText = `${lvivDescription}`;
     uniList.innerText = `UNIVERSITIES OF LVIV`;
     showUniversities(lvivUniversities, "universities-list");
 }
 modalKiev.onclick = function(){
     modal.style.display = 'block';
-    modalImg.style.backgroundImage = "url('../images/kiev.jpg')";  
+    modalImg.className = "the-modal-img in-kiev col-4 ";
+    // modalImg.style.background = "url('../images/kiev.jpg')";  
     cityDescription.innerText = `${kievDescription}`;
     uniList.innerText = `UNIVERSITIES OF KIEV`;
     showUniversities(kievUniversities, "universities-list");    
 }
 modalIvano.onclick = function(){
     modal.style.display = 'block';
-    modalImg.style.backgroundImage = "url('../images/ivano2.jpg')";   
+    modalImg.className = "the-modal-img in-ivano col-4 ";
+    // modalImg.style.background = "url('../images/ivano2.jpg')";   
     cityDescription.innerText = `${ivanoDescription}`;
     uniList.innerText = `UNIVERSITIES OF IVANO FRANKIVSK`; 
     showUniversities(ivanoUniversities, "universities-list");   
